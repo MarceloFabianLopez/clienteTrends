0fb6dadc58a96c48d8d58d9081f437e4f516cd1f
Marcelo Fabián López <cmflopez@gmail.com>
2020-09-26 07:14:26 -0300

    un poco mejor

--- src/componentes/CardArticle.js
+++ src/componentes/CardArticle.js
@@ -18,21 +18,27 @@ class CardArticle extends React.Component {
 
 
                 <Container>
-                    
+                <Card><Card.Header>
                <div className="tituloArticulo">
-                   
-               {this.props.titulo}</div>     
+                {this.props.titulo}</div></Card.Header>
+                <Card.Img variant="top" src={ this.props.articulosRelacionados[0].image ? this.props.articulosRelacionados[0].image.imageUrl:"/nodisponible.png"}/>
+                
                 <ListGroup className="list-group-flush">
                     {this.props.articulosRelacionados.map((articulo,i) =>{
                         
                         return (
-                        <ListGroupItem key={i}>
-                                <Card className="estiloArticulo">
+                        <ListGroupItem key={i}><div className="enlaceMedios">
+                            <a href={articulo.url} rel="noopener noreferrer">{articulo.source}</a></div>
+                            {/* { <Card>
+                              {/* <Card.Img variant="top" src={ articulo.image ? articulo.image.imageUrl:"#"}/> }
+                            <Card.Link href={articulo.url} target="_blank" rel="noopener noreferrer">{articulo.source}</Card.Link>
+                            </Card>} */}
+                               {/*  <Card className="estiloArticulo">
                                     <Card.Header><b>{articulo.source}</b></Card.Header>
                                     <Card.Img variant="top" src={ articulo.image ? articulo.image.imageUrl:"#"}/>
                                     <Card.Link href={articulo.url} target="_blank" rel="noopener noreferrer">{articulo.title}</Card.Link>
                                     
-                                </Card>
+                                </Card> */}
                             
                         </ListGroupItem>
                     
@@ -42,9 +48,11 @@ class CardArticle extends React.Component {
                     }
                     
                 </ListGroup>
+                
+                </Card>
                 </Container>
-                /* </Card.Body>
-            </Card> */
+                
+           
       );
     }
   }
